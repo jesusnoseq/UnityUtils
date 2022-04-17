91c6b9e360a5c65fe1d0d78dec3f57fe108bb3c7
jesusnoseq <jesusnoseq@hotmail.com>
2022-04-17 19:02:08 +0200

    feat(sound) : add sound utils

---
+++ Assets/com.github.jesusnoseq.unityutils/Runtime/Sound/MusicManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace com.jesusnoseq.util
+{
+
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(AudioSource))]
+    public class MusicManager : MonoBehaviourSingletonPersistent<MusicManager> {
+        AudioSource audioSource;
+        public AudioClip loopeableMusic;
+        // Use this for initialization
+        void Start () {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        private void Update()
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.clip = loopeableMusic;
+                audioSource.loop = true;
+                audioSource.Play();
+            }
+        }
+
+
+        }
+}
---
+++ Assets/com.github.jesusnoseq.unityutils/Runtime/Sound/SoundManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using System;
+
+namespace com.jesusnoseq.util
+{
+    public class SoundManager : MonoBehaviour
+    {
+        public AudioSource efxSource;                   //Drag a reference to the audio source which will play the sound effects.
+        public float lowPitchRange = .95f;              //The lowest a sound effect will be randomly pitched.
+        public float highPitchRange = 1.05f;            //The highest a sound effect will be randomly pitched.
+        public float pitchOffset=0;
+        public bool playOnEnable = false;
+
+        private float defaultPitch;
+
+        private void Start()
+        {
+            if (efxSource==null)
+            {
+                efxSource = GetComponent<AudioSource>();
+            }
+            defaultPitch = efxSource.pitch;
+        }
+
+        private void OnEnable()
+        {
+            if (playOnEnable)
+            {
+                efxSource.Play();
+            }
+        }
+
+
+
+
+        //Used to play single sound clips.
+        public void PlaySingle(AudioClip clip)
+        {
+            efxSource.pitch = defaultPitch;
+            efxSource.clip = clip;
+            efxSource.Play();
+        }
+
+
+
+        public void PlayRandomizeSfx(AudioClip clip){
+            float randomPitch = pitchOffset + UnityEngine.Random.Range(lowPitchRange, highPitchRange);
+            efxSource.pitch = randomPitch;
+            efxSource.clip = clip;
+
+            efxSource.Play();
+        }
+
+        //RandomizeSfx chooses randomly between various audio clips and slightly changes their pitch.
+        public void PlayRandomizeSfx(params AudioClip[] clips)
+        {
+            //Generate a random number between 0 and the length of our array of clips passed in.
+            int randomIndex = UnityEngine.Random.Range(0, clips.Length);
+            PlayRandomizeSfx(clips[randomIndex]);
+        }
+
+
+        public AudioClip GetLastSound()
+        {
+            return efxSource.clip;
+        }
+    }
+
+}
