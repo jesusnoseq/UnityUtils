aca4ea21399eb26d6486cc68e6797d2ad2e8a3cf
jesusnoseq <jesusnoseq@hotmail.com>
2022-04-20 01:11:25 +0200

    feat(editor): add unity editor scripts

---
+++ Assets/com.github.jesusnoseq.unityutils/Runtime/Editor/RegexAtribute.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+namespace com.jesusnoseq.util
+{
+    public class RegexAttribute : PropertyAttribute
+    {
+
+        public readonly string pattern;
+        public readonly string helpMessage;
+
+        public RegexAttribute(string pattern, string helpMessage)
+        {
+            this.pattern = pattern;
+            this.helpMessage = helpMessage;
+        }
+    }
+}
---
+++ Assets/com.github.jesusnoseq.unityutils/Runtime/Editor/SelectionBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace com.jesusnoseq.util
+{
+    [SelectionBase]
+    public class SelectionBase : MonoBehaviour
+    {
+
+    }
+}
