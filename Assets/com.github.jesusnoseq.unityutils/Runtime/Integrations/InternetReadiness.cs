69634e46cdb458458b1bfe5b1bbdb8ec73f230e2
jesusnoseq <jesusnoseq@hotmail.com>
2022-04-23 13:58:36 +0200

    feat(integrations): add internet readiness

---
+++ Assets/com.github.jesusnoseq.unityutils/Runtime/Integrations/InternetReadiness.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Events;
+
+namespace com.jesusnoseq.util
+{
+    public class InternetReadiness : MonoBehaviour, IReady
+    {
+        public string url = "http://clients3.google.com/generate_204";
+
+        private bool pingSended;
+        private bool pingOk;
+
+        public bool IsReady()
+        {
+            if (Application.internetReachability == NetworkReachability.NotReachable)
+            {
+                return false;
+            }
+            if (!pingSended)
+            {
+                pingSended = true;
+                StartCoroutine(Ping());
+            }
+            return pingOk;
+        }
+
+
+        IEnumerator Ping()
+        {
+            UnityWebRequest www = UnityWebRequest.Get(url);
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                pingOk = true;
+            }
+        }
+
+    }
+}
